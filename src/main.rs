c415c2ea5bb75315975a31e5c66d6201b02129c5
Khanh Duong <dqkqdlot@gmail.com>
2025-05-27 06:14:22 +0900

    3.3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health_check", get(|| async {}));
+    let app = Router::new().route("/health_check", get(health_check));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+async fn health_check() {}
