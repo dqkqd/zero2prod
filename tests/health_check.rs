3f91653ebd566b4daa620f3e4e2b3042cfc55ca2
Khanh Duong <dqkqdlot@gmail.com>
2025-05-28 06:35:53 +0900

    3.7.2

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,8 @@
 use axum::{
     body::Body,
-    http::{Request, StatusCode, header::CONTENT_LENGTH},
+    http::{self, Request, StatusCode, header::CONTENT_LENGTH},
 };
+use rstest::rstest;
 use tower::ServiceExt;
 use zero2prod::app;
 
@@ -21,4 +22,54 @@ async fn health_check_works() {
 
     assert_eq!(response.status(), StatusCode::OK);
     assert_eq!(response.headers().get(CONTENT_LENGTH).unwrap(), &"0");
+}
+
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    let app = app();
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/subscriptions")
+                .header(
+                    http::header::CONTENT_TYPE,
+                    mime::APPLICATION_WWW_FORM_URLENCODED.as_ref(),
+                )
+                .body(Body::from(
+                    "name=le%20guin&email=ursula_le_guin%40gmail.com",
+                ))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[rstest]
+#[case::missing_the_email("name=le%20guin")]
+#[case::missing_the_name("email=ursula_le_guin%40gmail.com")]
+#[case::missing_both_name_and_email("")]
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing(#[case] invalid_body: &'static str) {
+    let app = app();
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/subscriptions")
+                .header(
+                    http::header::CONTENT_TYPE,
+                    mime::APPLICATION_WWW_FORM_URLENCODED.as_ref(),
+                )
+                .body(Body::from(invalid_body))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
 }
