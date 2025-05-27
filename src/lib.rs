a0a21aaffe268278aa8d6a63faef3b844c50458d
Khanh Duong <dqkqdlot@gmail.com>
2025-05-28 06:45:08 +0900

    3.7.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,29 @@
-use axum::{Router, routing::get};
-
-pub fn app() -> Router {
-    Router::new().route("/health_check", get(health_check))
-}
-async fn health_check() {}
+use axum::{
+    Form, Router,
+    routing::{get, post},
+};
+use serde::Deserialize;
 
 pub async fn run(listener: tokio::net::TcpListener) -> std::io::Result<()> {
     let app = app();
     axum::serve(listener, app).await?;
     Ok(())
+}
+
+pub fn app() -> Router {
+    Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
+}
+
+async fn health_check() {}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct FormData {
+    name: String,
+    email: String,
+}
+async fn subscribe(Form(user): Form<FormData>) {
+    dbg!(user);
 }
