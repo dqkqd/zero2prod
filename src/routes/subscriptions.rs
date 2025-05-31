1e97894e0a6a00d725ccecdc19061244ccb942ea
Khanh Duong <dqkqdlot@gmail.com>
2025-05-31 17:46:18 +0900

    3.9.4: insert query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,15 +1,32 @@
-use axum::{Form, extract::State};
+use axum::{Form, extract::State, http::StatusCode};
+use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(Debug, Deserialize)]
-#[allow(dead_code)]
 pub struct FormData {
     name: String,
     email: String,
 }
 
 #[axum::debug_handler]
-pub async fn subscribe(State(pool): State<PgPool>, Form(user): Form<FormData>) {
-    dbg!(user, pool);
+pub async fn subscribe(
+    State(pool): State<PgPool>,
+    Form(form): Form<FormData>,
+) -> Result<(), StatusCode> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(&pool)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(())
 }
