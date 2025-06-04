9996f21a9983cd87708821cc0e9e94d71d670709
Khanh Duong <dqkqdlot@gmail.com>
2025-06-05 03:00:57 +0900

    4.5.9: cargo udeps

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
@@ -11,50 +12,46 @@
 
   outputs = {
     nixpkgs,
+    rust-overlay,
     flake-utils,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
 
-        sqlx-cli = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "sqlx-cli";
-          version = "0.8.6";
-          src = pkgs.fetchFromGitHub {
-            owner = "launchbadge";
-            repo = "sqlx";
-            rev = "refs/tags/v${version}";
-            hash = "sha256-Trnyrc17KWhX8QizKyBvXhTM7HHEqtywWgNqvQNMOAY=";
-          };
-          buildAndTestSubdir = "sqlx-cli";
-          cargoHash = "sha256-FxvzCe+dRfMUcPWA4lp4L6FJaSpMiXTqEyhzk+Dv1B8=";
-          buildNoDefaultFeatures = true;
-          buildFeatures = [
-            "postgres"
-          ];
-          doCheck = false;
-        };
+        rustNightly =
+          pkgs.rust-bin.selectLatestNightlyWith
+          (toolchain:
+            toolchain.default.override {
+              extensions = [
+                "rust-src"
+                "rustfmt"
+                "clippy"
+                "cargo"
+                "rust-analyzer"
+              ];
+            });
       in {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              bashInteractive
+          buildInputs = [
+            pkgs.openssl
+            pkgs.pkg-config
+            rustNightly
+          ];
+          packages = with pkgs; [
+            bashInteractive
 
-              # ci
-              act
+            # ci
+            act
 
-              # rust dev
-              cargo
-              clippy
-              rustc
-              rustfmt
-              rust-analyzer
+            # db
+            postgresql
 
-              # db
-              postgresql
-            ]
-            ++ [sqlx-cli];
+            sqlx-cli
+            cargo-udeps
+          ];
         };
       }
     );
