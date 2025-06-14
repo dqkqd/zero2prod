2f8cb9a74b7467a636ccf0c39524c8265a37c521
Khanh Duong <dqkqdlot@gmail.com>
2025-06-15 07:52:11 +0900

    6.13: email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,8 +22,25 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod test {
     use assertor::*;
+    use fake::locales::Data;
+    use quickcheck::Arbitrary;
+    use quickcheck_macros::quickcheck;
 
     use super::SubscriberEmail;
+
+    #[derive(Debug, Clone)]
+    struct ValitEmailFixture(String);
+
+    impl Arbitrary for ValitEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            // https://github.com/BurntSushi/quickcheck/issues/320#issuecomment-2085231712
+            // https://github.com/cksac/fake-rs/blob/2ab2ed66bf84f77cf0fbef7524f2c81e4066b6d5/fake/src/faker/impls/internet.rs#L46-L52
+            let username = g.choose(fake::locales::EN::NAME_FIRST_NAME).unwrap();
+            let domain = g.choose(&["com", "net", "org"]).unwrap();
+            let email = format!("{username}@example.{domain}");
+            ValitEmailFixture(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -41,5 +58,10 @@ mod test {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assertor::assert_that!(SubscriberEmail::parse(email)).is_err();
+    }
+
+    #[quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValitEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
